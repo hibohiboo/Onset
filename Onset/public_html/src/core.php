fcb4fb32a2f875008bfabbcfd9cf31966769d717
kiridaruma <www2480www@gmail.com>
2017-02-09 12:22:30 +0900

    add comment

--- Onset/public_html/src/core.php
+++ Onset/public_html/src/core.php
@@ -5,6 +5,11 @@ require_once(__DIR__.'/config.php');
 class Onset
 {
 
+    /*
+    バリデーション関数
+    外からの値はここを通してください
+    バグったときはここが原因の可能性が大
+    */
     public static function varidate(&$input)
     {
         $val = $input;
@@ -15,6 +20,10 @@ class Onset
         return $val;
     }
 
+    /*
+    部屋データを取得する関数
+    jsonをデコードしただけのstdClassが返ってきます
+    */
     public static function getRoomlist()
     {
         $name = RoomSavepath . 'roomlist';
@@ -24,6 +33,10 @@ class Onset
         return json_decode(file_get_contents($name));
     }
 
+    /*
+    部屋データを保存する関数
+    stdClassを渡してください(配列の場合はobjectでキャスト)
+    */
     public static function saveRoomlist(stdClass $roomlist)
     {
         $dir = RoomSavepath;
@@ -31,6 +44,10 @@ class Onset
         return $ret;
     }
 
+    /*
+    フロントへデータを渡すときはこれを使ってください
+    statusは、処理成功時は1、処理失敗時は-1を返してください
+    */
     public static function jsonMessage($message, $status = 1, $data = [])
     {
         $json = [
@@ -42,6 +59,10 @@ class Onset
         return json_encode($json);
     }
 
+    /*
+    ダイスロールをbcdiceで行うコマンド
+    通信周りはfile_get_contなので、もっと最適化できそう
+    */
     public static function diceroll($text, $sys)
     {
         $url = self::getBcdiceUrl();
@@ -56,6 +77,10 @@ class Onset
         return trim(str_replace('onset: ', '', $ret));
     }
 
+    /*
+    $_SERVER[]のデータをこねくり回してbcdiceへのURLを抽出する関数
+    よく分かんないから、むやみに触ると死にます
+    */
     public static function getBcdiceUrl()
     {
         if(BcdiceURL != "") return BcdiceURL;
@@ -67,6 +92,10 @@ class Onset
         return $procotlName . $_SERVER['SERVER_NAME'] . $urlPath;
     }
 
+    /*
+    チャットログを指定時間以降のものだけ切り取る関数(線形探索)
+    $timeはUnixTimestampで、0を渡すと処理を行わずに全件返します
+    */
     public static function searchLog(array $chatLog, $time){
         if($time == 0) return $chatLog;
         $point = count($chatLog) - 1;
@@ -76,7 +105,10 @@ class Onset
         else return [];
     }
 
-    //正直線形探索で十分と思うけど、念のため二分探索の関数もおいておきます
+    /*
+    チャットログを指定時間以降のものだけ切り取る関数(二分探索)
+    線形探索だけで十分だったので放置
+    */
     private static function binarySearch(array $chatLog, $time){
         $point = floor(count($chatLog) / 2);
         $width = $point;
@@ -88,7 +120,10 @@ class Onset
         return array_slice($chatLog, $point+1);
     }
 
-    //$roomPathは、語尾が'/'で終わっているパス名を引数に与えてください
+    /*
+    部屋を削除する関数
+    $roomPathは、語尾が'/'で終わっているパス名を引数に与えてください
+    */
     public static function removeRoomData($roomPath){
         $dirArr = scandir($roomPath);
         foreach($dirArr as $child){
@@ -102,6 +137,11 @@ class Onset
         if(!rmdir($roomPath)) throw new Exception("フォルダ".$roomPath."の削除に失敗しました");
     }
 
+    /*
+    部屋一覧を表示するhtmlを返す関数
+    はっきり言ってview側の処理なので、ここにこの関数があるのは設計的にどうなんだろう
+    何かいい案があれば教えてくだしあ
+    */
     public static function viewRoomlist(array $roomlist){
         $str = '';
         foreach($roomlist as $idx => $roomName){
