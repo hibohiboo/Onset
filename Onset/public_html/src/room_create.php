8b4f91abbbec41967c7a64ca89c61adc7732c0ac
kiridaruma <www2480www@gmail.com>
2017-02-23 20:58:52 +0900

    add room_create.php

---
+++ Onset/public_html/src/room_create.php
@@ -0,0 +1,32 @@
+<?php
+require_once __DIR__.'/autoload.php';
+use Onset;
+
+if(!Util::checkCsrfToken()){
+    echo Util::jsonMessage('不正なアクセス', -1);
+    exit();
+}
+
+$roomlist = Util::getRoomlist();
+
+$roomName = Util::getInput('roomName');
+$password = Util::getInput('password');
+
+if($roomName == null || $password == null){
+    echo Util::jsonMessage('空欄があります', -1);
+    exit();
+}
+
+if($roomlist->isRoomExist($roomName)){
+    echo Util::jsonMessage('すでに存在する部屋名です', -1);
+    exit();
+}
+
+$result = $roomlist->createRoom($roomName, $password);
+
+if(!$result){
+    echo Util::jsonMessage('部屋データの作成に失敗しました');
+    exit();
+}
+
+echo Util::jsonMessage('ok');
