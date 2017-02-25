60e62431a3ea22691b3e4dd3491ce9bf34bbfaa7
kiridaruma <www2480www@gmail.com>
2017-02-25 09:59:10 +0900

    add roomRemove.php

---
+++ Onset/public_html/src/roomRemove.php
@@ -0,0 +1,33 @@
+<?php
+require_once __DIR__.'/autoload.php';
+use Onset;
+
+if(!Util::checkCsrfToken()){
+    echo Util::jsonMessage('不正なアクセス', -1);
+    exit();
+}
+
+$roomlist = Util::getRoomlist();
+
+$roomName = Util::getInput('roomName');
+$password = Util::getInput('password');
+
+if($roomName == null || $password == null){
+    echo Util::jsonMessage('空欄があります', -1);
+    exit();
+}
+
+if(!$roomlist->isRoomExist($roomName)){
+    echo Util::jsonMessage('存在しない部屋名です', -1);
+    exit();
+}
+
+try{
+    $roomlist->removeRoom();
+}catch(\RuntimeException $err){
+    echo Util::jsonMessage($err->getMessage(), -1);
+    exit();
+}
+
+Util::saveRoomlist($roomlist);
+echo Util::jsonMessage('ok');
