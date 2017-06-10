0919cefcbaad5f7613740e79c218406fb356712a
kiridaruma <www2480www@gmail.com>
2017-06-10 20:30:10 +0900

    add
    フロントへのメッセージを担当するMessageクラスを追加

---
+++ Onset/public_html/src/lib/Message.php
@@ -0,0 +1,23 @@
+<?php
+namespace Onset;
+require_once __DIR__.'/../autoload.php';
+
+class Message{
+
+    static function ok($message, $data = []){
+        return static::messageJson(1, $message, $data);
+    }
+
+    static function err($message, $data = []){
+        return static::messageJson(0, $message, $data);
+    }
+
+    static function messageJson($code, $message, $data){
+        return json_encode([
+            'code'=>$code,
+            'message'=>$message,
+            'data'=>$data,
+        ]);
+    }
+
+}
