b567727334ac936f35dfb08d282071fc4437f10d
kiridaruma <www2480www@gmail.com>
2017-07-13 10:48:40 +0900

    change Room class

--- Onset/public_html/src/lib/Room.php
+++ Onset/public_html/src/lib/Room.php
@@ -7,71 +7,84 @@ require_once __DIR__.'/../autoload.php';
 */
 class Room{
 
-    public $id = '';
-    public $name = '';
-    private $passwd = '';
+    private $path;
+    private $roomlist;
 
-    // 部屋の実データが存在するフォルダへのパス(string)
-    private $path = '';
-
-    static function getList(){
-        $dir = realpath(RoomSavepath);
-        foreach(scandir($dir) as $fname){
-            if($fname === '.' || $fname === '..') continue;
-            if(is_dir($dir.'/'.$fname)) yield new static($fname);
-        }
+    function __construct(){
+        $this->path = realpath(\RoomSavepath);
+        $this->roomlist = json_decode(Util::loadFile($this->path . '/roomlist.json'), true);
     }
 
-    static function deleteOldRoom(){
-        foreach(scandir(RoomSavepath) as $file){
-            $path = realpath(RoomSavepath).'/'.$file;
-            if($file === '.' || $file === '..' || is_file($path)) continue;
-            if(time() - filemtime($path) > RoomDelTime){
+    public function getList(){
+        return $this->roomlist;
+    }
 
+    public function isExist($id){
+        return isset($this->roomlist[$id]);
+    }
+
+    public function find($id, $default = null){
+        if(!$this->isExist($id)) return $default;
+        return $this->roomlist[$id];
+    }
+
+    public function create($name, $pass){
+        $this->checkConfigLimit($name);
+
+        $id = uniqid(dechex(mt_rand()));
+        $result = $this->createDir($id);
+        if(!$result) throw new \RuntimeException('部屋データの初期化に失敗しました');
+
+        $hash = password_hash($pass, PASSWORD_DEFAULT);
+        $this->roomlist[$id] = ['name'=>$name, 'pass'=>$hash];
+        
+        $this->save();
+    }
+
+    public function delete($id){
+        if(!$this->isExist($id)) throw new \RuntimeException('存在しない部屋です');
+        unset($this->roomlist[$id]);
+        Util::removeFolder($this->path."/{$id}");
+        $this->save();
+    }
+
+    public function checkPassword($id, $pass){
+        if(!$this->isExist($id)) throw new \RuntimeException('存在しない部屋です');
+        $hash = $this->roomlist[$id]['pass'];
+        return \password_verify($pass, $hash);
+    }
+
+    public function getChatlog($id){
+        if(!$this->isExist($id)) throw new \RuntimeException('存在しない部屋です');
+        $logPath = $this->path . "/{$id}/log.json";
+        return new Chat($logPath);
+    }
+
+    public function deleteOldRoom(){
+        foreach($this->roomlist as $id => $data){
+            if(filemtime($this->path."/{$id}") + \RoomDelTime < time()){
+                $this->remove($id);
             }
         }
     }
 
-    function __construct($id = ''){
-        // 新規作成の場合、新しくフォルダを作成
-        if($id === ''){
-            if(scandir(RoomSavepath) > RoomLimit+2) throw new \RuntimeException('部屋数制限いっぱいです');
-            $this->id = uniqid('', true);
-            $this->path = realpath(RoomSavepath).'/'.$id;
-            mkdir($this->path);
-        }else{ // 読み込みの場合、存在するかチェックしてからデータ読み込み
-            $this->path = realpath(RoomSavepath).'/'.$id;
-            if(!file_exists($this->path)) throw new \RuntimeException('部屋が存在しません');
-            $this->id = $id;
-            $obj = json_decode(Util::loadFile($this->path.'/roomdata'));
-            $this->name = $obj->name;
-            $this->passwd = $obj->passwd;
-        }
+    private function createDir($id){
+        $dirPath = $this->path . "/{$id}/";
+        $result = mkdir($dirPath) &&
+            file_put_contents($dirPath.'log.json', '[]') &&
+            mkdir($dirPath.'connect');
+        
+        if(!$result) Util::removeFolder($dirPath);
+        return $result;
     }
 
-    public function save(){
-        if(mb_strlen($this->name) > MaxRoomName) throw new \RuntimeException('部屋名が長すぎます');
-        $data = (Object)[
-            'name'=>$this->name,
-            'passwd'=>$this->passwd,
-        ];
-        file_put_contents($this->path.'/roomdata', $data, LOCK_EX);
+    private function checkConfigLimit($name){
+        if(mb_strlen($name) > \MaxRoomName) throw new \RuntimeException('部屋名が長すぎます');
+        if(count($this->roomlist) > \RoomLimit) throw new \RuntimeException('部屋数制限いっぱいです');
     }
 
-    public function delete(){
-        Util::removeFolder($this->path);
+    private function save(){
+        $jsonPath = $this->path . '/roomlist.json';
+        file_put_contents($jsonPath, json_encode($this->roomlist), LOCK_EX);
     }
-
-    public function getChatlog(){
-        return new Chat($this->path.'/log.json');
-    }
-
-    public function checkPass($password) : bool{
-        return password_verify($password, $this->passwd);
-    }
-
-    public function setPassword($pass){
-        $this->passwd = password_hash($pass, PASSWORD_DEFAULT);
-    }
-
 }
